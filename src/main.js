a2aa2707ada49e29d1eb9d57a32c59cbee35bef6
bianjie <bj6546321>
2018-10-19 17:13:21 +0800

    vue

--- src/main.js
+++ src/main.js
@@ -2,28 +2,31 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
-import vueaxios from 'vue-axios'
 import axios from 'axios'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 import App from './App'
 import router from './router'
+import config from './config'
+// 导入iView UI
+Vue.use(iView)
+Vue.use(Vuex)
 
-
-axios.defaults.baseURL = config.api; // 设置axios的基础请求路径
-axios.defaults.timeout = 2000; // 设置axios的请求时间
+axios.defaults.baseURL = config.api // 设置axios的基础请求路径
+axios.defaults.timeout = 2000 // 设置axios的请求时间
 
 axios.loadData = async function (url) {
-  const resp = await axios.get(url);
-  return resp.data;
+  const resp = await axios.get(url)
+  return resp.data
 }
 
-Vue.prototype.$http = axios;// 将axios添加到 Vue的原型，这样一切vue实例都可以使用该对象
+Vue.prototype.$http = axios// 将axios添加到 Vue的原型，这样一切vue实例都可以使用该对象
 Vue.config.productionTip = false
-Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
---
+++ src/pages/Layout.vue
@@ -0,0 +1,90 @@
+<style scoped>
+  .layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+    height: 100vh
+  }
+  .layout-header-bar{
+    background: #fff;
+    box-shadow: 0 1px 1px rgba(0,0,0,.1);
+  }
+  .menu-item span{
+    display: inline-block;
+    overflow: hidden;
+    width: 69px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    vertical-align: bottom;
+    transition: width .2s ease .2s;
+  }
+  .menu-item i{
+    transform: translateX(0px);
+    transition: font-size .2s ease, transform .2s ease;
+    vertical-align: middle;
+    font-size: 16px;
+  }
+  .collapsed-menu span{
+    width: 0px;
+    transition: width .2s ease;
+  }
+  .collapsed-menu i{
+    transform: translateX(5px);
+    transition: font-size .2s ease .2s, transform .2s ease .2s;
+    vertical-align: middle;
+    font-size: 22px;
+  }
+</style>
+<template>
+  <div class="layout">
+    <Layout>
+      <Sider breakpoint="md" collapsible :collapsed-width="78" v-model="isCollapsed">
+        <Menu theme="dark" width="auto" :class="menuitemClasses">
+         <Submenu v-for="(item,index) in items" :key="item.title" :name="index"  >
+           <template slot="title">
+             <Icon :type="item.icon" />
+             {{item.title}}
+           </template>
+           <MenuItem v-for="(subitem) in item.items" :key="subitem.title" :to="subitem.path">{{subitem.title}}</MenuItem>
+         </Submenu>
+        </Menu>
+        <div slot="trigger"></div>
+      </Sider>
+      <Layout :style="{height:'100vh'}">
+        <Header class="layout-header-bar"></Header>
+        <Breadcrumb :style="{margin: '10px 0px 0px 10px'}">
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Components</BreadcrumbItem>
+          <BreadcrumbItem>Layout</BreadcrumbItem>
+        </Breadcrumb>
+        <Content :style="{margin: '10px', background: '#fff', minHeight: '220px'}">
+        </Content>
+      </Layout>
+    </Layout>
+  </div>
+</template>
+<script>
+import menus from '../menu'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      isCollapsed: false
+    }
+  },
+  computed: {
+    items () {
+      return menus
+    },
+    menuitemClasses: function () {
+      return [
+        'menu-item',
+        this.isCollapsed ? 'collapsed-menu' : ''
+      ]
+    }
+  }
+}
+</script>
